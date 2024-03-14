c16ea99ace3628a7df9ee17b58fbe904768d2b92
Achureggae <145215135+Achureggae@users.noreply.github.com>
2024-03-13 19:00:46 -0600

    Update 19110299_Adquirir_Conocimiento.py

--- 19110299_Adquirir_Conocimiento.py
+++ 19110299_Adquirir_Conocimiento.py
@@ -1,28 +1,36 @@
 import json
 
 class Conocimientos:
-    def __init__(self):
-        self.conocimiento = {
-            "Hola": "Hola! En que puedo ayudarte?",
-            "Como estas": "Bien, gracias, Y tu?",
-            "De que te gustaria hablar": "Puedo hablar de muchos temas! Que te interesa?"
-        }
+    def __init__(self, nombre_archivo="conocimiento.txt"):
+        if nombre_archivo:
+            self.cargar_conocimiento(nombre_archivo)
+        else:
+            self.conocimiento = {
+                "Hola": "Hola! En que puedo ayudarte?",
+                "Como estas": "Bien, gracias, Y tu?",
+                "De que te gustaria hablar": "Puedo hablar de muchos temas! Que te interesa?"
+            }
+
+    def cargar_conocimiento(self, nombre_archivo):
+        with open(nombre_archivo, "r") as archivo:
+            contenido = archivo.read()
+            self.conocimiento = json.loads(contenido)
 
     def ObtenerRespuesta(self, entrada):
         respuesta = self.conocimiento.get(entrada)
         if respuesta:
             return respuesta
         else:
-            return "No entendí la pregunta o no cuento con información, ¿me ayudas a entender?"
+            return "No entendí la pregunta o no cuento con informacion, ¿me ayudas a entender?"
 
     def AdquirirConocimiento(self, pregunta, resultado):
         self.conocimiento[pregunta] = resultado
         return "Conocimiento agregado correctamente."
 
-    def GuardarDiccionario(self):
-        Archivo = "Conocimiento.txt"
-        with open(Archivo, "w") as archivo:
-        json.dump(self.conocimiento, archivo)
+    def guardar_conocimiento(self, nombre_archivo):
+        with open(nombre_archivo, "w") as archivo:
+            json.dump(self.conocimiento, archivo)
+
 
 AdquirirConocimientos = Conocimientos()
 
@@ -35,7 +43,7 @@ def chat():
 
         if EntradaUsuario.lower() == 'adios':
             print("Hasta luego!")
-            AdquirirConocimientos.GuardarDiccionario
+            AdquirirConocimientos.guardar_conocimiento("conocimiento.txt")
             break
         else:
             print("Bot:", respuesta)
