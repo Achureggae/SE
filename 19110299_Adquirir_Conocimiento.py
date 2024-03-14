dc350e5d9db216c9a019ac61dd9a8c48e25215ab
Achureggae <145215135+Achureggae@users.noreply.github.com>
2024-03-13 18:40:03 -0600

    Update 19110299_Adquirir_Conocimiento.py

--- 19110299_Adquirir_Conocimiento.py
+++ 19110299_Adquirir_Conocimiento.py
@@ -1,8 +1,10 @@
+import json
+
 class Conocimientos:
     def __init__(self):
         self.conocimiento = {
             "Hola": "Hola! En que puedo ayudarte?",
-            "Como estas": "Estoy bien, gracias por preguntar. Y tu?",
+            "Como estas": "Bien, gracias, Y tu?",
             "De que te gustaria hablar": "Puedo hablar de muchos temas! Que te interesa?"
         }
 
@@ -11,11 +13,16 @@ class Conocimientos:
         if respuesta:
             return respuesta
         else:
-            return "Lo siento, no entendi. Podrias proporcionar mas detalles?"
+            return "No entendí la pregunta o no cuento con información, ¿me ayudas a entender?"
 
     def AdquirirConocimiento(self, pregunta, resultado):
         self.conocimiento[pregunta] = resultado
-        return "Nuevo conocimiento agregado correctamente."
+        return "Conocimiento agregado correctamente."
+
+    def GuardarDiccionario(self):
+        Archivo = "Conocimiento.txt"
+        with open(Archivo, "w") as archivo:
+        json.dump(self.conocimiento, archivo)
 
 AdquirirConocimientos = Conocimientos()
 
@@ -28,10 +35,11 @@ def chat():
 
         if EntradaUsuario.lower() == 'adios':
             print("Hasta luego!")
+            AdquirirConocimientos.GuardarDiccionario
             break
         else:
             print("Bot:", respuesta)
-            if respuesta == "Lo siento, no entendi. Podrias proporcionar mas detalles?":
+            if respuesta == "No entendí la pregunta o no cuento con informacion, ¿me ayudas a entender?":
                 PreguntaNueva = input("Bot: Cual es la pregunta adecuada para la respuesta? ")
                 RespuestaNueva = input("Bot: Cual es la respuesta a esa pregunta? ")
                 print(AdquirirConocimientos.AdquirirConocimiento(PreguntaNueva, RespuestaNueva))
